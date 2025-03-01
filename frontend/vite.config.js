f38aeb8af4eeb711edc6623bf4e806adbd30bcf5
horlamiedea <horlamiedea@gmail.com>
2025-03-01 22:53:42 +0100

    added another host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
-  plugins: [react],
+  plugins: [react()],
   server: {
-    port: 3000, 
-    host: true, 
+    port: 3000,
+    host: true,
+    allowedHosts: [
+      'healthcareapp.eastus.cloudapp.azure.com',
+    ],
   },
 });
